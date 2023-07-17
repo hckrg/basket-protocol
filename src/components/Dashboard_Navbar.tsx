9b9bb434159db9af67a949407f62aa521b362489
Harsh <harshagogia11@gmail.com>
2023-07-18 03:13:23 +0530

    fixed logo

--- src/components/Dashboard_Navbar.tsx
+++ src/components/Dashboard_Navbar.tsx
@@ -7,7 +7,7 @@ import { userAtom } from '../App';
 import * as fcl from '@onflow/fcl'
 import { getFlowBalance } from '../cadence/scripts/getFlowBalance';
 import { convertToTwoDecimalPlaces } from '../utils/constants';
-import flowLogo from "../images/flow.svg";
+import flowLogo from "../images/flow.jpeg";
 
 const Dashboard_Navbar = ({ navbarShadow }: { navbarShadow: boolean }) => {
   const [showLogout, setShowLogout] = useState(false);
@@ -69,13 +69,13 @@ const Dashboard_Navbar = ({ navbarShadow }: { navbarShadow: boolean }) => {
       </div>
       <div className="right-1 top-9 fixed flex justify-center items-center space-x-2 hover:opacity-80 cursor-pointer transition duration-200" onClick={handleImageClick}>
         <div>
-          {showLogout && (
+          {/* {showLogout && (
             <div className="absolute right-6 sm:right-56 rounded-lg shadow-lg my-1 border" ref={logoutRef}>
               <div className="flex space-x-2 px-2 sm:px-5 py-2 rounded-lg bg-white hover:bg-red-200 justify-center items-center">
                 <div className='opacity-0 left-0 top-0 absolute sm:static sm:opacity-100' onClick={handleLogout}>Logout</div>
               </div>
             </div>
-          )}
+          )} */}
         </div>
         {
           user?.loggedIn ? <div className="flex text-gray-500 text-sm opacity-0 items-center left-0 top-0 sm:pr-8 absolute sm:opacity-100 sm:static">
